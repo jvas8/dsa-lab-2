7158094554c190c218e32fb4954e698fc0cfd284
Jeimy Vasquez <2024161429@ub.edu.bz>
2025-09-27 17:56:37 +0000

    Implemented insert user with role parameter

--- lab2.cpp
+++ lab2.cpp
@@ -15,7 +15,25 @@ struct User {
         next = nullptr;
     }
 };
+    bool insertUser(User*& head, const string& username, const string& password, const string& role = "viewer") {
+        User* newUser = new User(username, password, role);
+        if (head == nullptr) {
+            head = newUser;
+            return true;
+        }
+        User* current = head;
+        while (current->next) {
+            if (current->username == username) return false; 
+            current = current->next;
+        }
+        current->next = newUser;
+        return true;
+    }
 
 int main() {
+    User* head = nullptr;
+    insertUser(head, "Jeimy", "pass123" , "admin");
+    insertUser(head, "Jane", "cats4ever", "editor");
+    insertUser(head, "Johnny", "joh12");
     return 0;
 }
