fa12a6bad11368437165749478eb50bff20c4e5b
Jeimy Vasquez <2024161429@ub.edu.bz>
2025-09-27 17:59:33 +0000

    Implemented findUser

--- lab2.cpp
+++ lab2.cpp
@@ -15,20 +15,32 @@ struct User {
         next = nullptr;
     }
 };
-    bool insertUser(User*& head, const string& username, const string& password, const string& role = "viewer") {
-        User* newUser = new User(username, password, role);
-        if (head == nullptr) {
-            head = newUser;
-            return true;
-        }
-        User* current = head;
-        while (current->next) {
-            if (current->username == username) return false; 
-            current = current->next;
-        }
-        current->next = newUser;
+bool insertUser(User*& head, const string& username, const string& password, const string& role = "viewer") {
+    User* newUser = new User(username, password, role);
+    if (head == nullptr) {
+        head = newUser;
         return true;
     }
+    User* current = head;
+    while (current->next) {
+        if (current->username == username) return false; 
+        current = current->next;
+    }
+    current->next = newUser;
+    return true;
+}
+User* findUser(User* head, const string& username) {
+    User* current = head;
+    while(current != nullptr){
+        if(current->username == username){
+            return current;
+        }
+        current = current->next;
+    }
+    return nullptr;
+}
+
+
 
 int main() {
     User* head = nullptr;
